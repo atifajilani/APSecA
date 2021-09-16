18b7f60b43ab8335c47b5768b1b9da3e076b9999
Atif <atif.jilani@nu.edu.pk>
2021-09-16 23:22:56 +0500

    THis is second Commit

---
+++ ApSecA/src/gitTest.java
@@ -0,0 +1,12 @@
+
+public class gitTest {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+	
+		System.out.println("Hello world");
+		System.out.println("This is how we do commit and push");
+
+	}
+
+}
