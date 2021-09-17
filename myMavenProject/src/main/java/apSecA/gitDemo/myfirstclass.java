3f006280ac08fb9329ae5872f8eb18ae8829aa3a
Atif <atif.jilani@nu.edu.pk>
2021-09-17 15:10:17 +0500

    Inlcuding Maven Project

---
+++ myMavenProject/src/main/java/apSecA/gitDemo/myfirstclass.java
@@ -0,0 +1,10 @@
+package apSecA.gitDemo;
+
+public class myfirstclass {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+        System.out.println("I am new to the world of Java");
+	}
+
+}
